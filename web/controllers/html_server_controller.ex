235e4e1e459d51ef06b6b0645854fbfc46edd8b4
Robert Butts <robert.o.butts@gmail.com>
2016-12-31 11:28:14 -0700

    Fix server html edit

--- web/controllers/html_server_controller.ex
+++ web/controllers/html_server_controller.ex
@@ -13,11 +13,11 @@ defmodule Tox.HtmlServerController do
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"server" => html_server_params}) do
-    changeset = Tox.Server.changeset(%Tox.Server{}, html_server_params)
+  def create(conn, %{"server" => server_params}) do
+    changeset = Tox.Server.changeset(%Tox.Server{}, server_params)
 
     case Repo.insert(changeset) do
-      {:ok, _html_server} ->
+      {:ok, _server} ->
         conn
         |> put_flash(:info, "Server created successfully.")
         |> redirect(to: html_server_path(conn, :index))
@@ -27,36 +27,36 @@ defmodule Tox.HtmlServerController do
   end
 
   def show(conn, %{"id" => id}) do
-    html_server = Repo.get!(Tox.Server, id)
-    render(conn, "show.html", html_server: html_server)
+    server = Repo.get!(Tox.Server, id)
+    render(conn, "show.html", server: server)
   end
 
   def edit(conn, %{"id" => id}) do
-    html_server = Repo.get!(Tox.Server, id)
-    changeset = Tox.Server.changeset(html_server)
-    render(conn, "edit.html", html_server: html_server, changeset: changeset)
+    server = Repo.get!(Tox.Server, id)
+    changeset = Tox.Server.changeset(server)
+    render(conn, "edit.html", server: server, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "html_server" => html_server_params}) do
-    html_server = Repo.get!(Tox.Server, id)
-    changeset = Tox.Server.changeset(html_server, html_server_params)
+  def update(conn, %{"id" => id, "server" => server_params}) do
+    server = Repo.get!(Tox.Server, id)
+    changeset = Tox.Server.changeset(server, server_params)
 
     case Repo.update(changeset) do
-      {:ok, html_server} ->
+      {:ok, server} ->
         conn
         |> put_flash(:info, "Server updated successfully.")
-        |> redirect(to: html_server_path(conn, :show, html_server))
+        |> redirect(to: html_server_path(conn, :show, server))
       {:error, changeset} ->
-        render(conn, "edit.html", html_server: html_server, changeset: changeset)
+        render(conn, "edit.html", server: server, changeset: changeset)
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    html_server = Repo.get!(Tox.Server, id)
+    server = Repo.get!(Tox.Server, id)
 
     # Here we use delete! (with a bang) because we expect
     # it to always work (and if it does not, it will raise).
-    Repo.delete!(html_server)
+    Repo.delete!(server)
 
     conn
     |> put_flash(:info, "Server deleted successfully.")
